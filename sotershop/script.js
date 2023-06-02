dd76d96a46e419dfdad470a12671caf11ac64a30
closeoffff <104249580+closeoffff@users.noreply.github.com>
2023-06-03 01:39:15 +0300

    Update script.js

--- sotershop/script.js
+++ sotershop/script.js
@@ -1,46 +1,46 @@
-let markNote = ['Dime', 'HP', 'Accer', 'Asus', 'Lenovo', 'Apple', ]
+let markNote = ['Dime', 'Stussy', 'Accer', 'Asus', 'Lenovo', 'Apple', ]
 
 let NoteApp = {
     data() {
         return {
             hover:false,
             hoverNumber:-1,
-            nameArr:['Футболки', 'Штани', 'Худі', 'Світшот', 'Головні убори', 'Сумки'],
+            nameArr:['Футболки', 'Штани', 'Худі', 'Світшот', 'Головні убори'],
             notebookArr:[
                 {   name: 'Футболки', 
                     brend: 'Dime',
                     material: '100% Cotton',
                     src: 'brands/TSHIRT_SU23_ADBLOCK_WHITE_600x600.png',
                     price: '2100',
-                    size:'s, m'
+                    size:'S, M'
                 },
                 {   name: 'Футболки', 
                 brend: 'Dime',
                 material: '100% Cotton',
                 src: 'brands/TSHIRT_SU23_SPLASH_WHITE_600x600.png',
                 price: '1900',
-                size:'s, m, l, xl'
+                size:'S, M, L, XL'
             },
             {   name: 'Футболки', 
             brend: 'Dime',
             material: '100% Cotton',
             src: 'brands/TSHIRT_SU23_SMALLLOGO_BLACK_600x600.png',
             price: '1700',
-            size:'s, m, l, xl'
+            size:'S, M, L, XL'
            },
            {   name: 'Футболки', 
            brend: 'Dime',
            material: '100% Cotton',
            src: 'brands/TSHIRT_SU23_XENO_GREENLAKE_600x600.png',
            price: '2000',
-           size:'m, l'
+           size:'M, L'
           },
           {   name: 'Футболки', 
            brend: 'Stussy',
            material: '100% Cotton',
            src: 'brands/stussytee1.jpg',
            price: '2500',
-           size:'s, m, l, xl, xxl (oversized fit)'
+           size:'S, M, L, Xl, XXL (oversized fit)'
           },
 
           {   name: 'Футболки', 
@@ -48,7 +48,7 @@ let NoteApp = {
           material: '100% Cotton',
           src: 'brands/stussytee2.jpg',
           price: '2200',
-          size:'s, m, l, xl(oversized fit)'
+          size:'S, M, L, XL(oversized fit)'
          },
 
 
@@ -62,7 +62,7 @@ let NoteApp = {
          material: '100% Cotton',
          src: 'brands/polartee1.jpg',
          price: '1900',
-         size:'s, m, l, xl'
+         size:'S, M, L, XL'
         },
 
         {   name: 'Футболки', 
@@ -70,7 +70,7 @@ let NoteApp = {
         material: '100% Cotton',
         src: 'brands/polartee2.jpg',
         price: '2100',
-        size:'l, xl'
+        size:'L, XL'
        },
 
 
@@ -153,6 +153,42 @@ let NoteApp = {
 
 
 
+       {   name: 'Штани', 
+       brend: 'Dime Jeans Dark Blue',
+       material: 'Denim',
+       src: 'brands/dimepants1.jpg',
+       price: '4300',
+       size:'S, M, L, Xl'
+      },
+
+      {   name: 'Штани', 
+      brend: 'Dime Jeans Light Blue',
+      material: 'Denim',
+      src: 'brands/dimepants2.jpg',
+      price: '4300',
+      size:'S, M, L, Xl'
+     },
+
+
+     {   name: 'Штани', 
+     brend: 'Dime Jeans Black',
+     material: 'Denim',
+     src: 'brands/dimepants3.jpg',
+     price: '4600',
+     size:'S, M, L, Xl'
+    },
+
+    {   name: 'Штани', 
+    brend: 'Dime Classic Navy Blue',
+    material: '100% Cotton',
+    src: 'brands/dimepants4.jpg',
+    price: '3300',
+    size:'S, M, L, Xl'
+   },
+
+
+
+
 
 
 
@@ -226,6 +262,261 @@ let NoteApp = {
       price: '3100',
       size:'M, L, Xl'
      },
+
+     {   name: 'Худі', 
+     brend: 'Stussy',
+     material: '100% Cotton',
+     src: 'brands/stussyhoodie1.jpg',
+     price: '4200',
+     size:'S,M, L, Xl'
+    },
+
+    {   name: 'Худі', 
+    brend: 'Stussy',
+    material: '100% Cotton',
+    src: 'brands/stussyhoodie2.jpg',
+    price: '3800',
+    size:'S, M, L, Xl'
+   },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   {   name: 'Світшот', 
+   brend: 'Stussy',
+   material: '100% Cotton',
+   src: 'brands/stussysweat1.jpg',
+   price: '3800',
+   size:'S, M, L, Xl'
+  },
+
+
+  {   name: 'Світшот', 
+   brend: 'Stussy x Nike',
+   material: '100% Cotton',
+   src: 'brands/stussysweat2.jpg',
+   price: '3300',
+   size:'S, M, L, Xl'
+  },
+
+  {   name: 'Світшот', 
+  brend: 'Stussy',
+  material: '100% Cotton',
+  src: 'brands/stussysweat4.jpg',
+  price: '2500',
+  size:'S, M, L, Xl'
+ },
+
+ {   name: 'Світшот', 
+ brend: 'Stussy',
+ material: '100% Cotton',
+ src: 'brands/stussysweat3.jpg',
+ price: '3600',
+ size:'S, M, L, Xl'
+},
+
+
+{   name: 'Світшот', 
+ brend: 'Dime',
+ material: '100% Cotton',
+ src: 'brands/dimesweat1.jpg',
+ price: '2600',
+ size:'S, M, L, Xl'
+},
+
+{   name: 'Світшот', 
+ brend: 'Dime',
+ material: '100% Cotton',
+ src: 'brands/dimesweat2.jpg',
+ price: '3200',
+ size:'S, M, L, Xl'
+},
+
+{   name: 'Світшот', 
+ brend: 'Dime',
+ material: '100% Cotton',
+ src: 'brands/dimesweat3.jpg',
+ price: '3100',
+ size:'S, M, L, Xl'
+},
+
+{   name: 'Світшот', 
+ brend: 'Dime',
+ material: '100% Cotton',
+ src: 'brands/dimesweat4.jpg',
+ price: '3600',
+ size:'S, M, L, Xl'
+},
+
+
+{   name: 'Світшот', 
+ brend: 'Fucking Awesome Fa',
+ material: '100% Cotton',
+ src: 'brands/fasweat1.jpg',
+ price: '2600',
+ size:'S, M, L, Xl'
+},
+
+{   name: 'Світшот', 
+ brend: 'Fucking Awesome Fa',
+ material: '100% Cotton',
+ src: 'brands/fasweat2.jpg',
+ price: '2800',
+ size:'S, M, L, Xl'
+},
+
+{   name: 'Світшот', 
+ brend: 'Fucking Awesome Fa',
+ material: '100% Cotton',
+ src: 'brands/fasweat4.jpg',
+ price: '3000',
+ size:'S, M, L, Xl'
+},
+
+{   name: 'Світшот', 
+ brend: 'Fucking Awesome Fa',
+ material: '100% Cotton',
+ src: 'brands/fasweat3.jpg',
+ price: '3800',
+ size:'S, M, L, Xl'
+},
+
+
+
+{   name: 'Головні убори', 
+ brend: 'Sicko Born From Pain',
+ material: 'Cotton',
+ src: 'brands/sickocap1.jpg',
+ price: '1200',
+ size:'One Size'
+},
+
+{   name: 'Головні убори', 
+ brend: 'Sicko Born From Pain',
+ material: 'Cotton',
+ src: 'brands/sickocap2.jpg',
+ price: '1300',
+ size:'One Size'
+},
+
+{   name: 'Головні убори', 
+ brend: 'Sicko Born From Pain',
+ material: 'Cotton',
+ src: 'brands/sickocap3.jpg',
+ price: '1300',
+ size:'One Size'
+},
+
+{   name: 'Головні убори', 
+ brend: 'Sicko Born From Pain',
+ material: 'Cotton',
+ src: 'brands/sickocap4.jpg',
+ price: '1300',
+ size:'One Size'
+},
+
+
+{   name: 'Головні убори', 
+ brend: 'Carhartt',
+ material: 'Cotton',
+ src: 'brands/carharttcap1.jpg',
+ price: '800',
+ size:'One Size'
+},
+
+{   name: 'Головні убори', 
+ brend: 'Carhartt',
+ material: 'Cotton',
+ src: 'brands/carharttcap2.jpg',
+ price: '1200',
+ size:'One Size'
+},
+
+{   name: 'Головні убори', 
+ brend: 'Carhartt',
+ material: 'Cotton',
+ src: 'brands/carharttcap3.jpg',
+ price: '1100',
+ size:'One Size'
+},
+
+
+{   name: 'Головні убори', 
+ brend: 'Carhartt',
+ material: 'Cotton',
+ src: 'brands/carharttcap4.jpg',
+ price: '1000',
+ size:'One Size'
+},
+
+
+{   name: 'Головні убори', 
+ brend: 'Supreme',
+ material: 'Cotton',
+ src: 'brands/supremecap1.jpg',
+ price: '2300',
+ size:'One Size'
+},
+
+{   name: 'Головні убори', 
+ brend: 'Supreme',
+ material: 'Cotton',
+ src: 'brands/supremecap2.jpg',
+ price: '2300',
+ size:'One Size'
+},
+
+{   name: 'Головні убори', 
+ brend: 'Supreme',
+ material: 'Cotton',
+ src: 'brands/supremecap3.jpg',
+ price: '2300',
+ size:'One Size'
+},
+
+{   name: 'Головні убори', 
+ brend: 'Supreme',
+ material: 'Cotton',
+ src: 'brands/supremecap4.jpg',
+ price: '2300',
+ size:'One Size'
+},
+
+
+
+
+
+
+
+
+
+     
 
 
 
